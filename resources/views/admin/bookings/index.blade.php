8390fda10805d9f566604b846cfb58a6601cef0e
akimAzman <s50708@ocean.umt.edu.my>
2023-01-29 09:45:57 +0800

    update for admin

--- resources/views/admin/bookings/index.blade.php
+++ resources/views/admin/bookings/index.blade.php
@@ -73,6 +73,48 @@
                                                 </form>
                                             </div>
                                         </td>
+                                        <td>
+                                            <label for="my-modal-6" class="btn btn-sm btn-primary text text-sm">Update
+                                                Status</label>
+                                            <!-- Put this part before </body> tag -->
+                                            <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                                            <div class="modal">
+                                                <div class="modal-box w-11/12 max-w-xs">
+                                                    <label for="my-modal-6"
+                                                        class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                                    <form method="POST"
+                                                        action="{{ route('bookings.updateStatus', $bs->id) }}">
+                                                        @csrf
+                                                        @method('PUT')
+                                                        <div>
+                                                            <input type="hidden" id="id" name="id" value="{{$bs->id}}"
+                                                                class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                                        </div>
+                                                        <label for="booking_status"
+                                                            class="block text-sm font-medium text-gray-700">Booking
+                                                            Status</label>
+                                                        <div class="mt-1">
+                                                            <select id="booking_status" name="booking_status"
+                                                                class="select select-sm select-bordered">
+                                                                <option value="Booking Receive"> Booking Receive </option>
+                                                                <option value="Pickup Car"> Pickup Car</option>
+                                                                <option value="Cancel"> Cancel</option>
+                                                                <option value="Payment Success">Payment Success</option>
+                                                                <option value="Payment Success">Payment Success</option>
+                                                                {{-- <option value="Success"> Success </option> --}}
+                                                            </select>
+                                                        </div>
+                                                        @error('booking_status')
+                                                        <div class="text-sm text-red-400">{{ $message }}</div>
+                                                        @enderror
+                                                        <div class="mt-6 p-4">
+                                                            <button type="submit"
+                                                                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg btn btn-sm text-white">Update</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </td>
 
                                     </tr>
                                     @empty
